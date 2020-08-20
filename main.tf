e5863e31dee37ed563a84a229ab17fb95ebc0697
chandradc <dcsekars_2000@yahoo.com>
2020-08-20 16:34:53 +0530

    First checkin

---
+++ main.tf
@@ -0,0 +1,18 @@
+
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = "us-east-1"
+
+}
+
+
+
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "chandradc-1040"
+  acl    = "private"
+  tags = {
+    env        = "dev"
+    created_by = "Chandra"
+  }
+}
